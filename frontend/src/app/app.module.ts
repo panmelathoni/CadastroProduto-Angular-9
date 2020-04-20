2f571721040360de66a9df87ba57e9baf2ac23b3
panmelathoni <panmelathoni@gmail.com>
2020-04-20 20:46:37 +0100

    backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,8 +21,9 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/products/product-create/product-create.component';
 
 import {MatButtonModule} from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
-
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -45,7 +46,9 @@ import {MatButtonModule} from '@angular/material/button';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent] 
